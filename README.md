8b95f69ed96d124bd38d9e2345bc694ae56eaf42
Grimm <grimm@necronomicode.com>
2024-11-09 15:06:48 +0100

    Add range extraction kata from codewars

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Python code challenges
-Code challenges solved using the Python programming language.
+Code challenges solved using the Python programming language. 
+
+## Codewars
+### 4 kyu
+- [Challenge](https://www.codewars.com/kata/51ba717bb08c1cd60f00002f) - [Code](codewars/range_extraction.py)
---
+++ codewars/range_extraction.py
@@ -0,0 +1,24 @@
+def solution(args):
+    result = []
+    i = 0
+
+    while i < len(args):
+        start = args[i]
+
+        while i+1 < len(args) and args[i] == args[i+1] - 1:
+            i += 1
+
+        end = args[i]
+
+        if end - start >= 2:
+            result.append(f"{start}-{end}")
+        
+        elif end - start == 1:
+            result.extend([str(start), str(end)])
+
+        else:
+            result.append(str(start))
+
+        i += 1
+
+    return ",".join(result)
